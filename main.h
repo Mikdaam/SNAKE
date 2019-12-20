3cf7210540c2e911c8d1409f84e33ff14cea7289
badarou <mikdaam.badarou@etu.upec.fr>
2019-12-20 05:01:05 +0100

    Upload files to ''

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED 
+
+	#define largeur 60
+	#define hauteur 40
+	#define CYCLE 1000000L
+	enum Direction{ PAUSE = 0, Gauche, Droite, Haut, Bas};
+	enum Direction dir;
+
+#endif /*MAIN_H_INCLUDED*/
